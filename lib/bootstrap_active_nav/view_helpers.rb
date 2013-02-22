47c3efecce7c175b85d3c6d686ad75c713afa2eb
Josh Kaplan <joshbrentkaplan@gmail.com>
2013-02-22 17:00:31 -0600

    Made option naming less verbose

--- lib/bootstrap_active_nav/view_helpers.rb
+++ lib/bootstrap_active_nav/view_helpers.rb
@@ -2,10 +2,10 @@ module BootstrapActiveNav
   module ViewHelpers
     def nav_link(link_text, link_path, options = {})
       options.reverse_merge!({
-        :active_class_name => "active",
-        :default_class_name => ""
+        :active => "active",
+        :default => ""
       })
-      class_name = current_page?(link_path) ? "#{options[:default_class_name]} #{options[:active_class_name]}" : options[:default_class_name]
+      class_name = current_page?(link_path) ? "#{options[:default]} #{options[:active]}" : options[:default]
 
       content_tag(:li, :class => class_name) do
         link_to link_path, options do
